153e7ab0c87ea49ac69745277f30f455aefb0545
hauhv_zyyx <hauhv@zyyx.jp>
2023-02-17 15:07:24 +0700

    update

--- resources/views/pages/learn/lesson.blade.php
+++ resources/views/pages/learn/lesson.blade.php
@@ -42,7 +42,7 @@
                     @else
                         <div class="lesson_card" id="{{ $item->p_language_id . $item->id }}" value="{{ $item->content }}"
                             lang="{{ $item->p_language_id }}"></div>
-                        @if ($item->compiler === 1)
+                        @if ($item->compiler == 1)
                             <div class="compiler_code_title">
                                 {{ $item->p_language_id != 'html' ? 'Compiler' : 'HTML Iframe' }}
                                 <div class="lds-ring">
