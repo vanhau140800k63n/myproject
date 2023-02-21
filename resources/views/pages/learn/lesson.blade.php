ef18233a819ddf59ba976befba2c8b396d391e41
hauhv_zyyx <hauhv@zyyx.jp>
2023-02-22 02:05:12 +0700

    update

--- resources/views/pages/learn/lesson.blade.php
+++ resources/views/pages/learn/lesson.blade.php
@@ -58,7 +58,7 @@
                 </div>
             </div>
             <div class="lesson_box_category_toggle"></div>
-            <div class="lesson_box_category_toggle_show"></div>
+            <div class="lesson_box_category_toggle_show"><i class="fa-solid fa-list"></i></div>
         </div>
         <div class="lesson_box_content">
             <div class="lesson_box_content_title">{{ $lesson->title }}</div>
