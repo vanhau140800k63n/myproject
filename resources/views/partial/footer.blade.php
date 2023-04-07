f21ffcb0f0d8aa7835c35f1b67431937bd0fe506
hauhv_zyyx <hauhv@zyyx.jp>
2023-04-07 10:34:23 +0700

    update

--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -1,4 +1,29 @@
-
 <div class="footer_box">
-    <div class="footer__copyright__text"><p> Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | DEVSNE.VN</p></div>
+    <div class="footer_info">
+        <div class="footer_logo">
+            <img style="width: 30px" src="{{ asset(mix('image/logo1.png')) }}">
+            <span style="">Nghề lập trình viên</span>
+        </div>
+        <div>
+            <h2>Về chúng tôi</h2>
+            <p>Giới thiệu</p>
+            <p>Liên hệ</p>
+            <p>Điều khoản</p>
+        </div>
+        <div>
+            <h2>Khóa học</h2>
+            <p>PHP</p>
+            <p>Python</p>
+            <p>JavaScript</p>
+            <p>Java</p>
+            <p>C++</p>
+        </div>
+    </div>
+    <div class="footer__copyright__text">
+        <p> Copyright &copy;
+            <script>
+                document.write(new Date().getFullYear());
+            </script> All rights reserved | DEVSNE.VN
+        </p>
+    </div>
 </div>
