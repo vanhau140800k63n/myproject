e8114d0c013c5e8cd7bcd61f2b15fda996dcc941
hauhv_zyyx <hauhv@zyyx.jp>
2023-04-08 14:33:41 +0700

    update

--- resources/views/admin/email/reset_password.blade.php
+++ resources/views/admin/email/reset_password.blade.php
@@ -1,20 +1,17 @@
-@extends('admin.master_auth')
-@section('content')
-    <span class="login-form-title">
-        Xác minh email
-    </span>
-    <div class="form-noti">
-        Xin chào <span>{{ $email }}, </span>
-        Vui lòng lựa chọn nút bên dưới để xác minh địa chỉ email Tài Khoản của bạn.
-        Việc xác minh địa chỉ email sẽ đảm bảo thêm một lớp bảo mật cho tài khoản của bạn.
-        Cung cấp thông tin chính xác sẽ giúp bạn nhận được hỗ trợ về tài khoản dễ dàng hơn khi cần.
+<span class="login-form-title">
+    Xác minh email
+</span>
+<div class="form-noti">
+    Xin chào <span>{{ $email }}, </span>
+    Vui lòng lựa chọn nút bên dưới để xác minh địa chỉ email Tài Khoản của bạn.
+    Việc xác minh địa chỉ email sẽ đảm bảo thêm một lớp bảo mật cho tài khoản của bạn.
+    Cung cấp thông tin chính xác sẽ giúp bạn nhận được hỗ trợ về tài khoản dễ dàng hơn khi cần.
+</div>
+<div class="container-login-form-btn">
+    <div class="wrap-login-form-btn">
+        <div class="login-form-bgbtn"></div>
+        <a class="login-form-btn" href="{{ route('change_password', ['user_id' => $user_id, 'token' => $token]) }}">
+            Đổi mật khẩu
+        </a>
     </div>
-    <div class="container-login-form-btn">
-        <div class="wrap-login-form-btn">
-            <div class="login-form-bgbtn"></div>
-            <a class="login-form-btn" href="{{ route('change_password', ['user_id' => $user_id, 'token' => $token]) }}">
-                Đổi mật khẩu
-            </a>
-        </div>
-    </div>
-@endsection
+</div>
