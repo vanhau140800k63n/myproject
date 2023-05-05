ca6fd2a1bd1f0134d03bcf4ed6fc99ef8c598f9a
hauhv_zyyx <hauhv@zyyx.jp>
2023-05-05 11:03:22 +0700

    update

--- resources/views/pages/post/detail.blade.php
+++ resources/views/pages/post/detail.blade.php
@@ -41,8 +41,8 @@
     @if ($theme == 1)
         <link rel="stylesheet" href="{{ asset(mix('lib/tinymce/skins/ui/oxide/content.min.css')) }}">
     @endif
-    <link rel="stylesheet" href="{{ asset(mix('css/update.css')) }}">
     @if ($theme == 2)
+        <link rel="stylesheet" href="{{ asset(mix('css/update.css')) }}">
         <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js"></script>
         <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/a11y-dark.min.css">
         <script>
