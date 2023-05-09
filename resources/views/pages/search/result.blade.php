7946357a79957def1e2bf70ff2cd7131c672a877
hauhv_zyyx <hauhv@zyyx.jp>
2023-05-09 11:49:51 +0700

    update

--- resources/views/pages/search/result.blade.php
+++ resources/views/pages/search/result.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 @section('meta')
     <meta name="description"
-        content="Devsne đã tổng hợp hơn 30 khóa học miễn phí về html, css, javascript, python, java, c++. Khóa học đi kèm luyện tập trực tuyến sẽ giúp bạn nhanh chóng cải thiện được khả năng lập trình">
+        content="30 khóa học miễn phí về html, css, javascript, python, java, c++. Cùng với hơn 1000 free template">
     <meta name="keywords"
         content="devsne, devsnevn, Miễn phí Lập trình Khóa học, php, java, python, c++, Cpp, HTML, css, javascript">
     <meta name="robots" content="index, follow">
@@ -9,7 +9,7 @@
     <meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">
 @endsection
 @section('head')
-    <title>{{ $key }} - devsnevn</title>
+    <title>Tìm hiểu về {{ $key }} | 30 Khóa Học Lập Trình Miễn Phí</title>
 @endsection
 @section('content')
     <div class="search_advance_box">
