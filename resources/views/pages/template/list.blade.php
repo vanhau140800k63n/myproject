ed9a81274ee2594b0931fb4fb74d1f0e17ee4c57
hauhv_zyyx <hauhv@zyyx.jp>
2023-05-09 10:37:45 +0700

    update

--- resources/views/pages/template/list.blade.php
+++ resources/views/pages/template/list.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.master')
 @section('meta')
-    <meta name="description"
-        content="Devsne đã tổng hợp hơn 30 khóa học miễn phí về html, css, javascript, python, java, c++. Khóa học đi kèm luyện tập trực tuyến sẽ giúp bạn nhanh chóng cải thiện được khả năng lập trình">
+    <meta name="description" content="Tổng hợp hơn 1000 template miễn phí sử dụng cho app, website">
+    <meta name="keywords"
+        content="devsne, devsnevn, website templates, templates, java, python, c++, cpp, html, css, javascript, free, free templates">
     <meta name="keywords"
         content="devsne, devsnevn, Miễn phí Lập trình Khóa học, php, java, python, c++, Cpp, HTML, css, javascript">
     <meta name="robots" content="index, follow">
@@ -9,7 +10,7 @@
     <meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1">
 @endsection
 @section('head')
-    <title> Danh sánh template {{ $key }} - devsnevn</title>
+    <title> 1000+ Free {{ $key }} template for website- devsnevn</title>
 @endsection
 @section('content')
     <div class="template_list_box">
