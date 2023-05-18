5cc26c478745d2fc4da30c07459a9e6e71dff804
hauhv_zyyx <hauhv@zyyx.jp>
2023-05-18 23:47:13 +0700

    update

--- resources/views/admin/email/register.blade.php
+++ resources/views/admin/email/register.blade.php
@@ -4,12 +4,12 @@
         Chào mừng {{ $email }}, đến với cộng đồng devsne. Để hoàn tất đăng ký, vui lòng nhấn vào link bên dưới:
     </div>
 
-    <div style="margin-top: 20px; height: 100px; text-align: center;">
+    <div style="height: 100px; text-align: center;">
         <a href="{{ route('change_password', ['user_id' => $user_id, 'token' => $token]) }}"
             style="text-decoration: none; background: linear-gradient(83.2deg, rgba(150, 93, 233, 1) 10.8%, rgba(99, 88, 238, 1) 94.3%);
         color: #fff; font-family: Google Sans,'Roboto',Arial;font-weight: bold;line-height: 44px;
         padding: 10px 20px; font-size: 22px;
-        border: none;
+        border: none; box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
         border-radius: 5px;">
             Đặt mật khẩu
         </a>
