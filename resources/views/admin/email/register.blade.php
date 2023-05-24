f4da3a179834e22753428d7470aff563a423e71f
hauhv_zyyx <hauhv@zyyx.jp>
2023-05-24 15:59:22 +0700

    update

--- resources/views/admin/email/register.blade.php
+++ resources/views/admin/email/register.blade.php
@@ -8,9 +8,9 @@
         <a href="{{ route('change_password', ['user_id' => $user_id, 'token' => $token]) }}"
             style="text-decoration: none; background: linear-gradient(83.2deg, rgba(150, 93, 233, 1) 10.8%, rgba(99, 88, 238, 1) 94.3%);
         color: #fff; font-family: Google Sans,'Roboto',Arial;font-weight: bold;line-height: 44px;
-        padding: 10px 20px; font-size: 22px;
+        padding: 10px 20px; font-size: 18px;
         border: none; box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
-        border-radius: 5px;">
+        border-radius: 25px; margin-top: 15px">
             Đặt mật khẩu
         </a>
     </div>
