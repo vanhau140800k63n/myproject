a443114c4ab87612988b2e40e8f0108e056fa257
hauhv_zyyx <hauhv@zyyx.jp>
2023-06-12 10:34:09 +0700

    update

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -76,15 +76,15 @@ class ExamController extends Controller
         $challenge = $this->challengeRepository->getChallengeWeek();
         $user = Auth::user();
 
-        // $time_start = strtotime($challenge->time_start);
-        // if($time_start > time()) {
-        //     return redirect()->back()->with('message', 'Bài Thi Chưa Bắt Đầu<br><span style="font-size: 16px; color:#ed5829">'. date('Y-m-d H:i:s', $time_start) . '</span>') ;
-        // }
+        $time_start = strtotime($challenge->time_start);
+        if($time_start > time()) {
+            return redirect()->back()->with('message', 'Bài Thi Chưa Bắt Đầu<br><span style="font-size: 16px; color:#ed5829">'. date('Y-m-d H:i:s', $time_start) . '</span>') ;
+        }
 
-        // $check_answer_exist = $this->challengeAnswerRepository->getAnswer($user->id, $challenge->id);
-        // if ($check_answer_exist != null) {
-        //     return redirect()->route('exam.challenge_weekly')->with('message', 'Hết Lượt Làm Bài');
-        // }
+        $check_answer_exist = $this->challengeAnswerRepository->getAnswer($user->id, $challenge->id);
+        if ($check_answer_exist != null) {
+            return redirect()->route('exam.challenge_weekly')->with('message', 'Hết Lượt Làm Bài');
+        }
 
         $data = [
             'user_id' => $user->id,
