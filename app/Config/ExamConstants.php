5cc91b464221d45a535c9a412936cc3d0a7596e2
hauhv_zyyx <hauhv@zyyx.jp>
2023-06-13 08:39:11 +0700

    update

--- app/Config/ExamConstants.php
+++ app/Config/ExamConstants.php
@@ -9,5 +9,5 @@ final class ExamConstants
     public const C_CURRENT = 0;
     public const C_RANKING = 1;
     public const C_DONE = 2;
-    public const SUBMIT_URL = 'https://990b-2405-4803-fbab-1660-1d96-4b7b-a242-9442.ngrok-free.app/test/test.php';
+    public const SUBMIT_URL = 'https://a706-2405-4803-fbab-1660-ac17-1a2c-28ea-2f98.ngrok-free.app/test/test.php';
 }
