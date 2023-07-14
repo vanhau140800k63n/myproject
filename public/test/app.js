abe9d0a0553868b3c5072a4a631d48e5d606025a
hauhv_zyyx <hauhv@zyyx.jp>
2023-07-14 15:21:00 +0700

    update

--- public/test/app.js
+++
@@ -1,8 +0,0 @@
-// This will use the demo backend if you open index.html locally via file://, otherwise your server will be used
-
-
-/**
- * Wrapper for client-side TikTok connection over Socket.IO
- * With reconnect functionality.
- */
- 
