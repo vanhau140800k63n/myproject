ac2df24690803010473c1fe3b9bcff9c9aa63710
hauhv_zyyx <hauhv@zyyx.jp>
2023-08-19 19:39:25 +0700

    update

--- public/exam_list/java/exercises/practice/acronym/src/main/java/Acronym.java
+++ public/exam_list/java/exercises/practice/acronym/src/main/java/Acronym.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 
 class Main {
+    
+
     public static void main(String args[]) {
         Scanner reader = new Scanner(System.in);
         String str = reader.nextLine();
-        System.out.print(1);
+        System.out.print(acronym(str));
     }
     public static String acronym(String str) {
-        return acronym(str);
+        return str;
     }
 }
