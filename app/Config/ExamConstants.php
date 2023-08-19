47c302f5f86517cf9db5dd1c0171bae2535bf648
hauhv_zyyx <hauhv@zyyx.jp>
2023-08-19 19:51:26 +0700

    update

--- app/Config/ExamConstants.php
+++ app/Config/ExamConstants.php
@@ -9,5 +9,5 @@ final class ExamConstants
     public const C_CURRENT = 0;
     public const C_RANKING = 1;
     public const C_DONE = 2;
-    public const SUBMIT_URL = 'https://56bc-2405-4802-1d80-5d60-a5b7-5ba3-5f2-d0f2.ngrok-free.app/';
+    public const SUBMIT_URL = 'https://40a6-2405-4802-1d80-5d60-a5b7-5ba3-5f2-d0f2.ngrok-free.app/';
 }
