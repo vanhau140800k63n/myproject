4dfe69037c7d03609049acdcf6a96876f9f6fe4c
vhau <vanhau140800@gmail.com>
2023-08-20 15:21:40 +0700

    update

--- app/Config/ExamConstants.php
+++ app/Config/ExamConstants.php
@@ -9,5 +9,5 @@ final class ExamConstants
     public const C_CURRENT = 0;
     public const C_RANKING = 1;
     public const C_DONE = 2;
-    public const SUBMIT_URL = 'https://f12f-2405-4802-1d80-5d60-91d3-c29a-7bd5-273a.ngrok-free.app/';
+    public const SUBMIT_URL = 'https://3cf9-2405-4802-1d80-5d60-4138-5b9e-eae6-4e1c.ngrok-free.app/';
 }
