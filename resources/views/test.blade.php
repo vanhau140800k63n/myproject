e1cfd18f0b11e6354180a57cc0ed3aa31c315e1a
hauhv <hauhv@zyyx.jp>
2023-12-21 14:57:54 +0700

    noel game

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -21,7 +21,7 @@
                                 </div>
                                 <span class="et__percent">{{ $json_data[$name] }}</span>
                             </div>
-                            <div class="et__progress" style="--w:{{ $json_data[$name]/count($json_data)*100 }};"></div>
+                            <div class="et__progress" style="--w:{{ $json_data[$name] }};"></div>
                         </label>
                     @endforeach
                     <button class="save"> Lưu lại</button>
