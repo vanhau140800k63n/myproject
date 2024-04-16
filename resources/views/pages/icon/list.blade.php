b2d3cfa5635fab7d1ed52e3da214638075ef1141
hauhv <hauhv@zyyx.jp>
2024-04-16 15:29:31 +0700

    update

--- resources/views/pages/icon/list.blade.php
+++ resources/views/pages/icon/list.blade.php
@@ -127,7 +127,7 @@
             <div class="icon_list">
                 @foreach ($iconList as $icon)
                     <article class="icon_list_item">
-                        <img src="{{ $icon->image }}"
+                        <img src="/{{ $icon->image }}"
                             width="64" height="64">
                         <button class="icon_download_btn"><i class="fa-regular fa-download fa-bounce"></i></button>
                     </article>
